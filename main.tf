da71d2a56d8306615ea71aa1413f2c947991354b
Trevor Powell <tpowell@hashicorp.com>
2025-02-21 23:04:06 -0800

    moving project_id around

--- main.tf
+++ main.tf
@@ -2,13 +2,12 @@
 # SPDX-License-Identifier: MPL-2.0
 
 provider "hcp" {
-
+  project_id  = var.hcp_project_id
 }
  
 resource "hcp_vault_secrets_app" "my-voice-is-my-passport" {
   app_name    = "my-voice-is-my-passport"
   description = "My new app!"
-  project_id  = var.hcp_project_id
 }
 resource "hcp_vault_secrets_secret" "secret-example" {
   app_name     = hcp_vault_secrets_app.my-voice-is-my-passport.app_name
