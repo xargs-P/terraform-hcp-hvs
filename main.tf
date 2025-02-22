83d2fa7864bf15e9de9c65c22c22f6c676661cf9
Trevor Powell <tpowell@hashicorp.com>
2025-02-21 17:38:02 -0800

    app name fix

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "hcp" {
 }
  
 resource "hcp_vault_secrets_app" "my-voice-is-my-passport" {
-  app_name    = "my voice is my passport"
+  app_name    = "my-voice-is-my-passport"
   description = "My new app!"
   project_id  = var.hcp_project_id
 }
