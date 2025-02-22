8deb9677a87c7a3d2f1741a9abfa88b75fcecdd6
Trevor Powell <tpowell@hashicorp.com>
2025-02-21 17:20:29 -0800

    project id variable

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ provider "hcp" {
 resource "hcp_vault_secrets_app" "my-voice-is-my-passport" {
   app_name    = "my voice is my passport"
   description = "My new app!"
+  project_id  = var.hcp_project_id
 }
 resource "hcp_vault_secrets_secret" "secret-example" {
   app_name     = hcp.hcp_vault_secrets_app.my-voice-is-my-passport.app_name
--- variables.tf
+++ variables.tf
@@ -1,6 +1,9 @@
 # Copyright (c) HashiCorp, Inc.
 # SPDX-License-Identifier: MPL-2.0
 
+variable "hcp_project_id" {
+  description = "ID of HCP Project to deploy module into."
+}
 /*variable "db_name" {
   description = "Unique name to assign to RDS instance"
 }
